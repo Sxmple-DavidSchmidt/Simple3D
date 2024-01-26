13027aba69edbbe7e599e9a12f53da5dd8c88f34
Sxmple-DavidSchmidt <damidtyt@gmail.com>
2024-01-26 15:31:14 +0100

    Adjusted default values for ControlPanel.java

--- src/gui/ControlPanel.java
+++ src/gui/ControlPanel.java
@@ -9,41 +9,26 @@ public class ControlPanel extends JPanel {
     public ControlPanel() {
         setLayout(new GridLayout(6, 0));
 
-        JSlider px = new JSlider(-50, 50, 0);
+        JSlider px = new JSlider(-50, 50, (int) Controller.CAMERA_POSITION_X);
         px.setPaintLabels(true);
         px.setPaintTicks(true);
         px.setMajorTickSpacing(25);
         px.setMinorTickSpacing(5);
-        px.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                Controller.CAMERA_POSITION_X = px.getValue();
-            }
-        });
+        px.addChangeListener(e -> Controller.CAMERA_POSITION_X = px.getValue());
 
-        JSlider py = new JSlider(-50, 50, 0);
+        JSlider py = new JSlider(-50, 50, (int) Controller.CAMERA_POSITION_Y);
         py.setPaintLabels(true);
         py.setPaintTicks(true);
         py.setMajorTickSpacing(25);
         py.setMinorTickSpacing(5);
-        py.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                Controller.CAMERA_POSITION_Y = py.getValue();
-            }
-        });
+        py.addChangeListener(e -> Controller.CAMERA_POSITION_Y = py.getValue());
 
-        JSlider pz = new JSlider(-50, 50, 0);
+        JSlider pz = new JSlider(-50, 50, (int) Controller.CAMERA_POSITION_Z);
         pz.setPaintLabels(true);
         pz.setPaintTicks(true);
         pz.setMajorTickSpacing(25);
         pz.setMinorTickSpacing(5);
-        pz.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                Controller.CAMERA_POSITION_Z = pz.getValue();
-            }
-        });
+        pz.addChangeListener(e -> Controller.CAMERA_POSITION_Z = pz.getValue());
 
         add(px);
         add(py);
@@ -54,36 +39,21 @@ public class ControlPanel extends JPanel {
         ox.setPaintTicks(true);
         ox.setMajorTickSpacing(60);
         ox.setMinorTickSpacing(30);
-        ox.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                Controller.CAMERA_ORIENTATION_X = ox.getValue() / 360.0;
-            }
-        });
+        ox.addChangeListener(e -> Controller.CAMERA_ORIENTATION_X = ox.getValue() / 360.0);
 
         JSlider oy = new JSlider(-180, 180, 0);
         oy.setPaintLabels(true);
         oy.setPaintTicks(true);
         oy.setMajorTickSpacing(60);
         oy.setMinorTickSpacing(30);
-        oy.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                Controller.CAMERA_ORIENTATION_Y = oy.getValue() / 360.0;
-            }
-        });
+        oy.addChangeListener(e -> Controller.CAMERA_ORIENTATION_Y = oy.getValue() / 360.0);
 
         JSlider oz = new JSlider(-180, 180, 0);
         oz.setPaintLabels(true);
         oz.setPaintTicks(true);
         oz.setMajorTickSpacing(60);
         oz.setMinorTickSpacing(30);
-        oz.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                Controller.CAMERA_ORIENTATION_Z = oz.getValue() / 360.0;
-            }
-        });
+        oz.addChangeListener(e -> Controller.CAMERA_ORIENTATION_Z = oz.getValue() / 360.0);
 
         add(ox);
         add(oy);
--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -3,7 +3,7 @@ package gui;
 public class Controller {
     public static double CAMERA_POSITION_X = 0;
     public static double CAMERA_POSITION_Y = 0;
-    public static double CAMERA_POSITION_Z = -10;
+    public static double CAMERA_POSITION_Z = -20;
     public static double CAMERA_ORIENTATION_X;
     public static double CAMERA_ORIENTATION_Y;
     public static double CAMERA_ORIENTATION_Z;
