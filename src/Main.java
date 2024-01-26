0eb485ac3c26ec5ab6b696a18a6454a56a400998
CuttingEdge <62205605+Sxmple-DavidSchmidt@users.noreply.github.com>
2024-01-26 15:23:16 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,7 @@
-import gui.Window;
-
-import javax.swing.*;
-import java.awt.*;
+import gui.UserInterface;
 
 public class Main {
     public static void main(String[] args) {
-        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-
-        Window w = new Window();
-        w.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
-        JFrame frame = new JFrame();
-        frame.setTitle("3D-Project");
-        frame.setSize(d.width / 2, d.height / 2);
-        frame.setLocation(d.width / 4, d.height / 4);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.setVisible(true);
-
-        frame.add(w, BorderLayout.CENTER);
-        frame.add(new JSlider(), BorderLayout.WEST);
-
-        new Thread(() -> {
-            while (true) {
-                frame.repaint();
-                try {
-                    Thread.sleep(15);
-                } catch (InterruptedException ex) {
-                    System.out.println("Failed to sleep thread");
-                }
-            }
-        }).start();
+        UserInterface ui = new UserInterface();
     }
 }
