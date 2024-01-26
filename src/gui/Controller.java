f479d323d763aa61d277e9f1806c02e79c15dc60
CuttingEdge <62205605+Sxmple-DavidSchmidt@users.noreply.github.com>
2024-01-26 15:21:00 +0100

    Create Controller.java

---
+++ src/gui/Controller.java
@@ -0,0 +1,10 @@
+package gui;
+
+public class Controller {
+    public static double CAMERA_POSITION_X = 0;
+    public static double CAMERA_POSITION_Y = 0;
+    public static double CAMERA_POSITION_Z = -10;
+    public static double CAMERA_ORIENTATION_X;
+    public static double CAMERA_ORIENTATION_Y;
+    public static double CAMERA_ORIENTATION_Z;
+}
