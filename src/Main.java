0d8e20cf921796826d6064f0d8cf15f4df2c2e61
Sxmple-DavidSchmidt <damidtyt@gmail.com>
2024-02-07 13:24:25 +0100

    Implemented loading .obj objects

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,20 @@ import gui.Controller;
 import gui.UserInterface;
 import util.Vec3;
 import world.World;
-import world.objects.Cube;
 import world.objects.ObjObject;
-import world.objects.Orientation;
 import world.objects.Pyramid;
-
-import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
         World world = new World();
-        world.addObject(new Pyramid(new Vec3(0, 0, 0), 5));
-        world.addObject(new Cube(new Vec3(10, 0, -50), 5));
-        world.addObject(new ObjObject(Main.class.getResource("ressources/uploads_files_3757122_mig-29.obj").getPath()));
+        world.addObject(new Pyramid(new Vec3(0, 0, 100), 5));
+        world.addObject(new ObjObject(
+                Main.class.getResource("ressources/uploads_files_3757122_mig-29.obj").getPath(),
+                new Vec3(0, 20, -100),
+                new Vec3(0, 0, 0)
+        ));
 
         Controller.world = world;
-        UserInterface ui = new UserInterface();
+        new UserInterface();
     }
 }
